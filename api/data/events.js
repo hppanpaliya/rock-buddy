7261442683839b3d27a52c12ddaf147266cce807
Frank Appolonia <fappolonia@gmail.com>
2022-12-07 17:08:04 -0500

    routes and search for events

--- api/data/events.js
+++ api/data/events.js
@@ -2,24 +2,41 @@ const axios = require('axios');
 require('dotenv').config();
 
 /** URL Config */
-let baseUrl = `https://app.ticketmaster.com/discovery/v2/events?apikey=${process.env.TM_API_KEY}`
+let url = `https://app.ticketmaster.com/discovery/v2/events?apikey=${process.env.TM_API_KEY}`
 let ts = new Date();
 let day = ts.getDate();
 if (day < 10) day = "0" + day
 ts = ts.getFullYear() + "-" + (ts.getMonth()+1) + "-" + day + "T00:00:00Z";
-baseUrl += `&locale=en-us&startDateTime=${ts}&classificationName=rock&size=50`;
+const baseUrl = url + `&locale=en-us&startDateTime=${ts}&classificationName=rock&size=50`;
 
 async function displayUpcomingEvents(page=0){
 
     //1. validate
-    if(arguments.length !== 0) throw "Invalid number of arguments!";
+    if(arguments.length !== 1) throw "Invalid number of arguments!";
     
     // 2. Query api
-    baseUrl += `&page=${page}` + `&sort=date,asc`
-    const data = await axios.get(baseUrl);
+    let apiUrl = baseUrl + `&page=${page}` + `&sort=date,asc`
+    const data = await axios.get(apiUrl);
+
+    //3. return
+    return data.data;
+};
+
+async function searchEvents(keyword, page=0){
+
+    //1. validate
+    if(arguments.length !== 2) throw "Invalid number of arguments!";
+    
+    // 2. Query api
+    let apiUrl = baseUrl + `&keyword=${keyword}` + `&page=${page}` + `&sort=date,asc`
+    const data = await axios.get(apiUrl);
+
+    //3.return
     return data.data;
 };
 
 module.exports = {
-    displayUpcomingEvents
+    displayUpcomingEvents,
+    searchEvents
 }
+
