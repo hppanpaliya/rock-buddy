12fdf558f40f208b22468f25007f21c0c270c129
hppanpaliya <hppanpaliya@gmail.com>
2022-12-17 23:03:00 -0500

    Realtime Chat with MUI Tags

---
+++ client/src/components/firebase/Chat/UserList.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { TextField, List, ListItemText,ListItemButton } from "@mui/material";
+import { useState } from "react";
+
+const UserList = ({ users, selectedUser, setSelectedUser, searchText, handleSearch }) => {
+  const [hoveredItem, setHoveredItem] = useState(null);
+
+  const handleMouseEnter = (user) => {
+    setHoveredItem(user);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredItem(null);
+  };
+
+  const filteredUsers = users.filter((user) => user.username.toLowerCase().includes(searchText.toLowerCase()));
+
+  return (
+    <div className="user-list">
+      <TextField label="Search users" value={searchText} onChange={handleSearch} />
+      <List>
+        {filteredUsers.map((user) => (
+          <ListItemButton
+            key={user.uid}
+            selected={user === hoveredItem || user === selectedUser}
+            onClick={() => setSelectedUser(user)}
+            onMouseEnter={() => handleMouseEnter(user)}
+            onMouseLeave={handleMouseLeave}
+          >
+            <ListItemText primary={user.username} />
+          </ListItemButton>
+        ))}
+      </List>
+    </div>
+  );
+};
+
+export default UserList;
