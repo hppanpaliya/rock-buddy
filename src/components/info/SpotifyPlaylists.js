cdbfc038b10cac3545f8ba1c8c077147244e768e
hppanpaliya <hppanpaliya@gmail.com>
2022-12-20 04:36:31 -0500

    Added Modal to Display Playlist Tracks.

--- src/components/info/SpotifyPlaylists.js
+++ src/components/info/SpotifyPlaylists.js
@@ -4,8 +4,8 @@ import axios from "axios";
 import { useSelector } from "react-redux";
 import SignIn from "../firebase/SignIn";
 import Card from "@mui/material/Card";
-import { CardMedia, Typography } from "@mui/material";
-import { CardContent,Grid } from "@mui/material";
+import { CardMedia, Modal, Typography } from "@mui/material";
+import { CardContent, Grid } from "@mui/material";
 import { Box } from "@mui/system";
 import rock from "../rock.png";
 import Player from "./Player";
@@ -15,19 +15,20 @@ import IconButton from "@mui/material/IconButton";
 import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import SkipNextIcon from "@mui/icons-material/SkipNext";
-
+import CloseIcon from "@mui/icons-material/Close";
 
 const SpotifyPlayLists = () => {
   const [playlists, setPlayLists] = useState([]);
   const [tracks, setTracks] = useState([]);
-  const [playingTrack,setPlayingTracks] = useState([]);
+  const [playingTrack, setPlayingTracks] = useState([]);
+  const [open, setOpen] = useState(false);
   const auth = useSelector((state) => state.auth || null);
   const dispatch = useDispatch();
   let offset = 0;
   let limit = 50;
   let hasNextPage = true;
   let playlist = [];
-  
+
   let token = window.sessionStorage.getItem("token");
 
   useEffect(() => {
@@ -56,12 +57,9 @@ const SpotifyPlayLists = () => {
   }, [token]);
 
   async function playlistItems(id) {
-    const { data } = await axios.get(
-      `https://api.spotify.com/v1/playlists/${id}/tracks`,
-      {
-        headers: { Authorization: `Bearer ${token}` },
-      }
-    );
+    const { data } = await axios.get(`https://api.spotify.com/v1/playlists/${id}/tracks`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
     //console.log(data.items);
     setTracks(data.items);
   }
@@ -87,28 +85,31 @@ const SpotifyPlayLists = () => {
   const buildPlaylistCard = (playlist) => {
     return (
       <Grid item xs={2} sm={4} md={4} key={playlist.id}>
-        <Card >
-          <CardMedia
-            component={"img"}
-            alt={"an image from Unsplash"}
-            image={playlist.images.length !== 0 ? playlist.images[0].url : rock}
-          />
+        <Card>
+          <CardMedia component={"img"} alt={"an image from Unsplash"} image={playlist.images.length !== 0 ? playlist.images[0].url : rock} />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
               <code> Playlist Name: {playlist.name}</code>
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              <code>
-                {playlist.description.length > 100
-                  ? playlist.description.substring(0, 100) + "..."
-                  : playlist.description}
-              </code>
+              <code>{playlist.description.length > 100 ? playlist.description.substring(0, 100) + "..." : playlist.description}</code>
             </Typography>
           </CardContent>
           <Box mx="auto">
             {playlist.tracks.total !== 0 ? (
+              /* 
               <Button
                 onClick={() => playlistItems(playlist.id)}
+                variant="contained"
+              >
+                <code>Open Playlist</code>
+              </Button>
+              */
+              <Button
+                onClick={() => {
+                  setOpen(true);
+                  playlistItems(playlist.id);
+                }}
                 variant="contained"
               >
                 <code>Open Playlist</code>
@@ -120,7 +121,7 @@ const SpotifyPlayLists = () => {
           <br />
           <br />
         </Card>
-        </Grid>
+      </Grid>
     );
   };
 
@@ -128,69 +129,83 @@ const SpotifyPlayLists = () => {
     console.log("Clicked Play");
     const hash = window.location.hash;
     let spotifyUri = window.sessionStorage.getItem("spotifyUri");
-    if(spotifyUri !== uri){
-      window.location.hash = ""
+    if (spotifyUri !== uri) {
+      window.location.hash = "";
       dispatch(setTrackID(spotifyUri));
-      window.sessionStorage.setItem("spotifyUri", uri)
+      window.sessionStorage.setItem("spotifyUri", uri);
     }
     //setPlayerTrack(uri);
   };
 
-  if (!(tracks.length > 0)) {
-    return (
-      <Box sx={{maxWidth: '75%', marginLeft: 'auto', marginRight: 'auto'}}>
+  return (
+    <div>
+      <Box sx={{ maxWidth: "75%", marginLeft: "auto", marginRight: "auto" }}>
         <Typography gutterBottom variant="p" component="div">
-                    <h1> User Playlists</h1>
-                    <br/>
-                    <br/>
-                  </Typography>
-        <Grid container spacing={{xs: 2, mid: 3}} columns={{xs: 4, sm: 8, md: 12}}>
+          <h1> User Playlists</h1>
+          <br />
+          <br />
+        </Typography>
+        <Grid container spacing={{ xs: 2, mid: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
           {console.log("inside grid")}
-        {playlists && playlists.map((playlist) => buildPlaylistCard(playlist))}
+          {playlists && playlists.map((playlist) => buildPlaylistCard(playlist))}
         </Grid>
       </Box>
-    );
-  } else {
-    console.log("Tracks already");
-    return (
-      <div>
-        { tracks.map((item) => {
+
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        style={{
+          overflowY: "auto",
+          position: "absolute",
+
+          backgroundColor: "#f2f2f2",
+        }}
+        fullScreen
+      >
+        <div>
+          <IconButton style={{ position: "sticky", top: 0}} onClick={() => setOpen(false)}>
+            <CloseIcon sx={{ fontSize: 40 }} />
+          </IconButton>
+          {tracks.map((item) => {
             console.log(item);
             return (
               <div>
-        <Card sx={{maxWidth: '100%', marginLeft: '15%', marginRight: '15%', verticalAlign: "top", minHeight: '10vh'}}>
-        <Box sx={{
-				maxWidth: '800%', marginLeft: '10', marginRight: '10',
-				display: 'flex', flexDirection: 'row',   alignItems: "center",
-				justifyContent: "center", verticalAlign: "top"
-					
-				}}>
-          <CardContent sx={{ flex: "1 0 auto", justifyContent: "left"}}>
-            <Typography component="div" variant="h5">
-            {item.track.name}
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              color="text.secondary"
-              component="div"
-            >
-             {item.track.artists.length > 1? "Various Artists" : item.track.artists[0].name}
-            </Typography>
-          </CardContent>
-          <Box sx={{maxWidth: '50%', marginLeft: 'auto', marginRight: '5%'}}>
-            <IconButton aria-label="play/pause" onClick={() => setPlayerTrack(item.track.uri)}
-                    variant="contained">
-              <PlayArrowIcon sx={{ height: 38, width: 38 }} />
-            </IconButton>
-          </Box>
-        </Box>
-      </Card>
-      <br/>
-      </div>
-      );
-      })}
-      </div> 
-      )}
+                <Card sx={{ maxWidth: "100%", marginLeft: "15%", marginRight: "15%", verticalAlign: "top", minHeight: "10vh" }}>
+                  <Box
+                    sx={{
+                      maxWidth: "800%",
+                      marginLeft: "10",
+                      marginRight: "10",
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      verticalAlign: "top",
+                    }}
+                  >
+                    <CardContent sx={{ flex: "1 0 auto", justifyContent: "left" }}>
+                      <Typography component="div" variant="h5">
+                        {item.track.name}
+                      </Typography>
+                      <Typography variant="subtitle1" color="text.secondary" component="div">
+                        {item.track.artists.length > 1 ? "Various Artists" : item.track.artists[0].name}
+                      </Typography>
+                    </CardContent>
+                    <Box sx={{ maxWidth: "50%", marginLeft: "auto", marginRight: "5%" }}>
+                      <IconButton aria-label="play/pause" onClick={() => setPlayerTrack(item.track.uri)} variant="contained">
+                        <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+                      </IconButton>
+                    </Box>
+                  </Box>
+                </Card>
+                <br />
+              </div>
+            );
+          })}
+        </div>
+      </Modal>
+    </div>
+  );
 };
 
 export default SpotifyPlayLists;
